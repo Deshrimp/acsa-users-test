83fa0f5669feee48f3213e895787b8b152861829
Jessica Y <desh365@hotmail.com>
2019-02-25 01:51:09 -0600

    seeding a default admin user

--- schema.sql
+++ schema.sql
@@ -14,9 +14,16 @@ CREATE TABLE `usersProfile`(
   `nombre` VARCHAR (50) NOT NULL,
   `edad` Int (3) NOT NULL,
   `username` VARCHAR (30) NOT NULL,
-  `img` VARCHAR (255) NOT NULL,
   `mail` VARCHAR (100) NOT NULL,
   `rol` VARCHAR (20) NOT NULL,
    PRIMARY KEY ( `id` ) 
 
 )
+INSERT INTO usersInfos (username, password, createdAt, updatedAt)
+VALUES ("admin", "Abc123456.", "2019-02-25 05:54:12", "2019-02-25 05:54:12");
+
+INSERT INTO userProfiles (nombre, edad, username, correo, rol, createdAt, updatedAt)
+VALUES("Arcsa", 1, "admin", "arcsa@gmail.com", "admin", "2019-02-25 05:54:12", "2019-02-25 05:54:12");
+
+SELECT * from usersInfos;
+-- SELECT * from userProfiles
