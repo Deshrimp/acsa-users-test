b7139b1adc687a231443336084ebd602311dd8a9
Deshrimp <desh365@hotmail.com>
2019-03-28 11:23:40 -0600

    use object for sequlelize set up

--- schema.sql
+++ schema.sql
@@ -4,28 +4,32 @@ USE users;
 
 CREATE TABLE `usersInfo` (
 `id` Int (11) AUTO_INCREMENT NOT NULL,
-  `username` VARCHAR (30) NOT NULL,
+  `name` VARCHAR (30) NOT NULL,
   `password` VARCHAR (60) NOT NULL,
-   PRIMARY KEY ( `id` ) 
+   PRIMARY KEY ( `id` )
 
 );
 CREATE TABLE `usersProfile`(
   `id` Int (11) AUTO_INCREMENT NOT NULL,
-  `nombre` VARCHAR (50) NOT NULL,
-  `edad` Int (3) NOT NULL,
-  `username` VARCHAR (30) NOT NULL,
-  `mail` VARCHAR (100) NOT NULL,
-  `rol` VARCHAR (20) NOT NULL,
-   PRIMARY KEY ( `id` ) 
+  `name` VARCHAR (50) NOT NULL,
+  `age` Int (3) NOT NULL,
+  `gender` VARCHAR (30) NOT NULL,
+  `role` VARCHAR (100) NOT NULL,
+  `code` VARCHAR (20) NOT NULL,
+   PRIMARY KEY ( `id` )
+);
+INSERT INTO usersInfo (name, password)
+VALUES ("admin", "Abc123456-");
 
-)
-INSERT INTO usersInfos (username, password, createdAt, updatedAt)
-VALUES ("admin", "Abc123456.", "2019-02-25 05:54:12", "2019-02-25 05:54:12");
+INSERT INTO usersProfile (name, age, gender, role, code)
+VALUES("admin", 1, "male", "admin", "1234567891");
 
-INSERT INTO userProfiles (nombre, edad, username, correo, rol, createdAt, updatedAt)
-VALUES("Arcsa", 1, "admin", "arcsa@gmail.com", "admin", "2019-02-25 05:54:12", "2019-02-25 05:54:12");
+SELECT * from usersInfo;
+SELECT * from usersProfile;
 
-SELECT * from usersInfos;
--- SELECT * from userProfiles
--- "username": "adminn",
+-- "username": "admin",
 -- "password": "Abc456789-"
+
+-- USE IN CASE OF EMERGENCY: Unhandled rejection SequelizeConnectionError: Client does not support authentication protocol requested by server; consider upgrading MySQL client
+-- ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';
+-- ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
--- sequelize.js
+++ sequelize.js
@@ -1,27 +1,40 @@
-const Sequelize = require("sequelize")
-const usersInfo = require("./models/usersInfo")
-const usersProf = require("./models/usersProfile")
+const Sequelize = require('sequelize');
+const usersInfo = require('./models/usersInfo');
+const usersProf = require('./models/usersProfile');
 
-const sequelize = new Sequelize("users", "root", "password", {
-  host: "localhost",
-  dialect: "mysql",
-  pool: {
-    max: 10,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-})
+const configmysql = {
+	database: process.env.PSQL_DB_NAME,
+	username: process.env.PSQL_DB_USER,
+	password: process.env.PSQL_DB_PASS,
+	host: process.env.PSQL_DB_HOST,
+	port: process.env.PSQL_DB_PORT,
+	dialect: 'postgres',
+	timezone: '+00:00',
+	logging: s => debug(s)
+};
 
-const usersInformation = usersInfo(sequelize, Sequelize)
-const usersProfile = usersProf(sequelize, Sequelize)
+// const sequelize = new Sequelize('users', 'root', 'password', {
+// 	host: 'localhost',
+// 	dialect: 'mysql',
+// 	pool: {
+// 		max: 10,
+// 		min: 0,
+// 		acquire: 30000,
+// 		idle: 10000
+// 	}
+// });
+
+const sequelize = new Sequelize(configmysql);
+
+const usersInformation = usersInfo(sequelize, Sequelize);
+const usersProfile = usersProf(sequelize, Sequelize);
 
 //usersInformation.belongsTo(usersProfile)
 sequelize.sync({ force: false }).then(() => {
-  console.log("Database and tables created")
-})
+	console.log('Database and tables created');
+});
 
 module.exports = {
-  usersInformation,
-  usersProfile
-}
+	usersInformation,
+	usersProfile
+};
