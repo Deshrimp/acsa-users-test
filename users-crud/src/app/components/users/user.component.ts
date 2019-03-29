f7e3c864563ff563d55f78df5cf63f9d1c73012a
Jessica Y <desh365@hotmail.com>
2019-03-28 22:49:22 -0600

    fixed errors

--- users-crud/src/app/components/users/user.component.ts
+++ users-crud/src/app/components/users/user.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm, FormsModule } from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
-import { UsersService } from '../../services/users.service';
-import { User } from '../../interfaces/user.interface';
+import { Component, OnInit } from "@angular/core"
+import { NgForm, FormsModule } from "@angular/forms"
+import { Router, ActivatedRoute } from "@angular/router"
+import { UsersService } from "../../services/users.service"
+import { User } from "../../interfaces/user.interface"
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
+  selector: "app-user",
+  templateUrl: "./user.component.html",
   styles: [
     `
       .ng-invalid.ng-touched:not(form) {
@@ -17,52 +17,55 @@ import { User } from '../../interfaces/user.interface';
 })
 export class UserComponent implements OnInit {
   user: User = {
-    name: '',
-    age: '',
-    gender: '',
-    password: '',
-    code: '',
-    role: '',
+    name: "",
+    age: null,
+    gender: "",
+    password: "",
+    code: "",
+    role: "",
     id: 0
-  };
-  new: boolean = false;
-  id: string;
-  constructor(private _UsersService: UsersService, private router: Router, private route: ActivatedRoute) {
+  }
+  new: boolean = false
+  id: string
+  constructor(
+    private _UsersService: UsersService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
     this.route.params.subscribe(parametros => {
-      console.log(parametros);
-      this.id = parametros['id'];
-      if (this.id != 'nuevo') {
-        this._UsersService.getUser(this.id).subscribe((user: User) => (this.user = user));
+      console.log(parametros)
+      this.id = parametros["id"]
+      if (this.id != "nuevo") {
+        this._UsersService
+          .getUser(this.id)
+          .subscribe((user: User) => (this.user = user))
       }
-    });
+    })
   }
 
   ngOnInit() {}
 
-  guardar(forma:NgForm) {
-
-  console.log("ngForm ",forma );
-  console.log("Valor forma", forma.value  );
-
-  console.log("Usuario", this.usuario );
-    if (this.id == 'nuevo') {
+  guardar(forma: NgForm) {
+    console.log("ngForm ", forma)
+    console.log("Valor forma", forma.value)
+    if (this.id == "nuevo") {
       this._UsersService.newUser(this.user).subscribe(
         (data: { userProfile: User }) => {
-          this.router.navigate(['/user', data.userProfile.id]);
+          this.router.navigate(["/user", data.userProfile.id])
         },
         error => console.error(error)
-      );
+      )
     } else {
       this._UsersService.updateUser(this.user, this.id).subscribe(
         (data: { userProfile: User }) => {
-          console.log("this is the data that I'm sending", data);
+          console.log("this is the data that I'm sending", data)
         },
         error => console.error(error)
-      );
+      )
     }
   }
   addNew(forma: NgForm) {
-    this.router.navigate(['/user', 'nuevo']);
-    forma.reset();
+    this.router.navigate(["/user", "nuevo"])
+    forma.reset()
   }
 }
